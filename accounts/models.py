d9e6d4cf072ee32795d7459c025ede3d8249c724
metalcode03 <metalcode03@gmail.com>
2020-02-10 01:29:26 +0100

    minor changes

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,20 @@
+import unicodedata
+
+from django.core.validators import RegexValidator
 from django.contrib.auth.models import (AbstractBaseUser, AbstractUser,
                                         BaseUserManager)
-from django.core.validators import RegexValidator
-from django.db import models
-from django.utils.translation import gettext_lazy as _
-from location_app.models import Location
 
+from django.contrib.auth import password_validation
+from django.contrib.auth.hashers import (
+    check_password, is_password_usable, make_password,
+)
+
+from django.db import models
+
+from django.utils.translation import gettext_lazy as _
+from django.utils.crypto import get_random_string, salted_hmac
+
+from location_app.models import Location
 # Create your models here.
 USERNAME_REGEX = '^[a-zA-Z0.+-]*$'
 
@@ -87,7 +97,7 @@ class UserManager(BaseUserManager):
 
 
 # USER MAIN CLASS MODELS
-class User(AbstractBaseUser):
+class User(models.Model):
     
     SEXCHOICE = (
         ('MA', _('Male')),
@@ -142,6 +152,99 @@ class User(AbstractBaseUser):
         choices=SEXCHOICE,
         default=SEXCHOICE[2]
     )
+    password = models.CharField(_('password'), max_length=128)
+    last_login = models.DateTimeField(_('last login'), blank=True, null=True)
+
+    is_active = True
+
+    REQUIRED_FIELDS = []
+
+    # Stores the raw password if set_password() is called so that it can
+    # be passed to password_changed() after the model is saved.
+    _password = None
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.get_username()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self._password is not None:
+            password_validation.password_changed(self._password, self)
+            self._password = None
+
+    def get_username(self):
+        """Return the username for this User."""
+        return getattr(self, self.USERNAME_FIELD)
+
+    def clean(self):
+        setattr(self, self.USERNAME_FIELD,
+                self.normalize_username(self.get_username()))
+
+    def natural_key(self):
+        return (self.get_username(),)
+
+    @property
+    def is_anonymous(self):
+        """
+        Always return False. This is a way of comparing User objects to
+        anonymous users.
+        """
+        return False
+
+    @property
+    def is_authenticated(self):
+        """
+        Always return True. This is a way to tell if the user has been
+        authenticated in templates.
+        """
+        return True
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+        self._password = raw_password
+
+    def check_password(self, raw_password):
+        """
+        Return a boolean of whether the raw_password was correct. Handles
+        hashing formats behind the scenes.
+        """
+        def setter(raw_password):
+            self.set_password(raw_password)
+            # Password hash upgrades shouldn't be considered password changes.
+            self._password = None
+            self.save(update_fields=["password"])
+        return check_password(raw_password, self.password, setter)
+
+    def set_unusable_password(self):
+        # Set a value that will never be a valid hash
+        self.password = make_password(None)
+
+    def has_usable_password(self):
+        """
+        Return False if set_unusable_password() has been called for this user.
+        """
+        return is_password_usable(self.password)
+
+    def get_session_auth_hash(self):
+        """
+        Return an HMAC of the password field.
+        """
+        key_salt = "django.contrib.auth.models.AbstractBaseUser.get_session_auth_hash"
+        return salted_hmac(key_salt, self.password).hexdigest()
+
+    @classmethod
+    def get_email_field_name(cls):
+        try:
+            return cls.EMAIL_FIELD
+        except AttributeError:
+            return 'email'
+
+    @classmethod
+    def normalize_username(cls, username):
+        return unicodedata.normalize('NFKC', username) if isinstance(username, str) else username
 
     # FOR VERIFICATION AND PERMISSION
     active = models.BooleanField(default=True)
