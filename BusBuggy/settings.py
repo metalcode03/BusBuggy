a832fa218663b4f424d75a39707759692f7870e8
metalcode03 <metalcode03@gmail.com>
2020-02-14 04:50:26 +0100

    better static

--- BusBuggy/settings.py
+++ BusBuggy/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_env')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
