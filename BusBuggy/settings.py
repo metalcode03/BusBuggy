f6f8e672c42f91c25879f8f6068c159c5adc11bd
metalcode03 <metalcode03@gmail.com>
2020-02-17 02:45:29 +0100

    god grace

--- BusBuggy/settings.py
+++ BusBuggy/settings.py
@@ -131,8 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files
 
 STATIC_URL = '/statics/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
